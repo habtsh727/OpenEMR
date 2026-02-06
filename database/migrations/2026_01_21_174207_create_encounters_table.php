829b76fe1df52f5c7921d3b4c1c38ed7b96f7db4
habtsh727export <habtsh727@gmail.comexport>
2026-02-06 17:01:29 +0300

    old vital removed

--- database/migrations/2026_01_21_174207_create_encounters_table.php
+++ database/migrations/2026_01_21_174207_create_encounters_table.php
@@ -20,12 +20,6 @@ return new class extends Migration
             $table->foreignIdFor(User::class, 'doctor_id')->nullable()->constrained()->nullOnDelete();
             $table->foreignIdFor(User::class, 'triage_by')->nullable()->constrained()->nullOnDelete();
             $table->foreignIdFor(CardPayment::class)->nullable()->constrained()->nullOnDelete();
-            // $table->string('bp_systolic')->nullable();
-            // $table->string('bp_diastolic')->nullable();
-            // $table->string('temperature')->nullable();
-            // $table->string('pulse')->nullable();
-            // $table->string('spo2')->nullable();
-            // $table->string('priority')->nullable();
             $table->string('processed_by')->nullable();
             $table->enum('status', ['pending', 'triaged', 'doctor_assigned','in_progress', 'completed', 'cancelled'])->default('pending');
             $table->timestamps();
