65a5ec7ab74151f5e5b379f06092ac49819dc5d1
habtsh727export <habtsh727@gmail.comexport>
2026-02-18 22:09:34 +0300

    registration-report-fixed

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,4 +75,8 @@ class User extends Authenticatable // implements MustVerifyEmail
     {
         return $this->hasMany(Referral::class, 'referring_doctor_id');
     }
+    public function processedPayments()
+    {
+        return $this->hasMany(CardPayment::class, 'processed_by');
+    }
 }
