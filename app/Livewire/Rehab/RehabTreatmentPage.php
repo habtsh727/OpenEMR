8f832b8fc939fd9413770056fce1afe6140bb90d
habtsh727export <habtsh727@gmail.comexport>
2026-02-26 09:47:17 +0300

    rehabTreatmetnLivewire-added

---
+++ app/Livewire/Rehab/RehabTreatmentPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Rehab;
+
+use Livewire\Component;
+
+class RehabTreatmentPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.rehab.rehab-treatment-page');
+    }
+}
