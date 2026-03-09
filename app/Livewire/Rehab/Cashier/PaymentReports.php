f6df7205432903c778abcc29a9c740762828538b
habtsh727export <habtsh727@gmail.comexport>
2026-03-09 10:14:54 +0300

    rehab-payment-report-added

---
+++ app/Livewire/Rehab/Cashier/PaymentReports.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Rehab\Cashier;
+
+use Livewire\Component;
+
+class PaymentReports extends Component
+{
+    public function render()
+    {
+        return view('livewire.rehab.cashier.payment-reports');
+    }
+}
