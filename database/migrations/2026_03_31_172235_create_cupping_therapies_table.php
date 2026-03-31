8121f6c0b9fa67a4b27e014dc39496872f086598
habtsh727export <habtsh727@gmail.comexport>
2026-03-31 20:25:25 +0300

    all cupping-migrations-done

---
+++ database/migrations/2026_03_31_172235_create_cupping_therapies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cupping_therapies', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('encounter_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->text('notes')->nullable();
+
+            // Summary amounts
+            $table->decimal('total_amount', 10, 2)->default(0);
+            $table->decimal('discount', 10, 2)->default(0);
+            $table->decimal('final_amount', 10, 2)->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cupping_therapies');
+    }
+};
